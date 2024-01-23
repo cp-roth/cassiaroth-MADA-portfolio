b7619a925524f45768bf49207d6d5bf242346e8f
cp-roth <cassia.roth@gmail.com>
2024-01-23 13:47:01 -0500

    23 Jan 2024 Updated Part 1 of R Coding Exercise

---
+++ coding-exercise/README.md.R
@@ -0,0 +1,19 @@
+# My Project
+
+This is a brief description of my project.
+
+## Installation
+
+To install this project, follow these steps:
+  
+  1. Clone the repository.
+2. Run `install.packages("my_package")` in R to install the required package.
+
+## Usage
+
+To use this project, run the following R code:
+  
+```{R}
+library(my_package)
+my_function()
+``
